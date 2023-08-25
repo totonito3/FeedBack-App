cd26843ae365ef30377b223f39b92971dade2e9f
Bertin Tamegnon <btamegnon3@gmail.com>
2023-08-25 11:37:57 -0400

    Added uuid package to manage ids, handled feedback submit functions,  rating component...

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 
@@ -8,6 +9,13 @@ import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+    console.log(newFeedback);
+  };
+
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete? ")) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -19,11 +27,12 @@ function App() {
     <>
       <Header></Header>
       <div className="container">
-        <FeedbackForm></FeedbackForm>
+        <FeedbackForm handleAdd={addFeedback}></FeedbackForm>
         <FeedbackStats feedback={feedback}></FeedbackStats>
         <FeedbackList
           feedback={feedback}
-          handleDelete={deleteFeedback}></FeedbackList>
+          handleDelete={deleteFeedback}
+        ></FeedbackList>
       </div>
     </>
   );
